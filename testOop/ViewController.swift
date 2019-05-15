794f6cc7575b9cd80233a82b9e8d92c89c7858ce
Dimitrij <dimitrijfajar@yahoo.co.uk>
2019-05-15 08:16:52 +0700

    Initial Commit

---
+++ testOop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testOop
+//
+//  Created by Dimitrij Fajar Satria Dharma on 15/05/19.
+//  Copyright © 2019 Dimitrij Fajar Satria Dharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
